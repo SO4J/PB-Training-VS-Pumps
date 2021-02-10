ad865109af9edda621d9a40796a14a70664b1a28
Joshua Bolding <78222519+SO4J@users.noreply.github.com>
2021-02-10 17:50:38 -0500

    Add files via upload
    Initial Upload

---
+++ script.js
@@ -0,0 +1,151 @@
+//Current syntax to show element on click
+$(document).ready(function(){
+  $("#header1").click(function(){
+    $("#text1").show();
+  });
+});
+
+$(document).ready(function(){
+    $("#header2").click(function(){
+      $("#text2").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header3").click(function(){
+      $("#text3").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header4").click(function(){
+      $("#text4").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header5").click(function(){
+      $("#text5").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header6").click(function(){
+      $("#text6").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header7").click(function(){
+      $("#text7").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header8").click(function(){
+      $("#text8").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header9").click(function(){
+      $("#text9").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header10").click(function(){
+      $("#text10").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header11").click(function(){
+      $("#text11").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header12").click(function(){
+      $("#text12").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#pumpAffinityTitle").click(function(){
+      $("#pumpAffinityPic").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#motorTechnologyTitle").click(function(){
+      $("#motorTechnologyPic").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#pumpPayingBackTitle").click(function(){
+      $("#pumpPayingBackPic").show();
+    });
+  });
+
+//Current syntax to hide an element after page loads
+$(function () {
+    $('#text1').hide();
+});
+
+$(function () {
+    $('#text2').hide();
+});
+
+$(function () {
+    $('#text3').hide();
+});
+
+$(function () {
+    $('#text4').hide();
+});
+
+$(function () {
+    $('#text5').hide();
+});
+
+$(function () {
+    $('#text6').hide();
+});
+
+$(function () {
+    $('#text7').hide();
+});
+
+$(function () {
+    $('#text8').hide();
+});
+
+$(function () {
+    $('#text9').hide();
+});
+
+$(function () {
+    $('#text10').hide();
+});
+
+$(function () {
+    $('#text11').hide();
+});
+
+$(function () {
+    $('#text12').hide();
+});
+
+$(function () {
+    $('#pumpAffinityPic').hide();
+});
+
+$(function () {
+    $('#motorTechnologyPic').hide();
+});
+
+$(function () {
+    $('#pumpPayingBackPic').hide();
+});
