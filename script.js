4bcf65b3dd7b025b224ba76fb80a682af5fa842d
Joshua Bolding <78222519+SO4J@users.noreply.github.com>
2021-02-12 21:24:03 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,111 @@
+//Click training mode button to hide elements
+$(document).ready(function(){
+  $("#trainingButton").click(function(){
+    $("#text1").hide();
+    $('#text2').hide();
+    $('#text3').hide();
+    $('#text4').hide();
+    $('#text5').hide();
+    $('#text6').hide();
+    $('#text7').hide();
+    $('#text8').hide();
+    $('#text9').hide();
+    $('#text10').hide();
+    $('#text11').hide();
+    $('#text12').hide();
+    $('#pumpAffinityPic').hide();
+    $('#motorTechnologyPic').hide();
+    $('#pumpPayingBackPic').hide();
+  });
+});
+
+//Click to show elements
+$(document).ready(function(){
+  $("#header1").click(function(){
+    $("#text1").show();
+  });
+});
+
+$(document).ready(function(){
+    $("#header2").click(function(){
+      $("#text2").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header3").click(function(){
+      $("#text3").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header4").click(function(){
+      $("#text4").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header5").click(function(){
+      $("#text5").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header6").click(function(){
+      $("#text6").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header7").click(function(){
+      $("#text7").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header8").click(function(){
+      $("#text8").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header9").click(function(){
+      $("#text9").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header10").click(function(){
+      $("#text10").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header11").click(function(){
+      $("#text11").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#header12").click(function(){
+      $("#text12").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#pumpAffinityTitle").click(function(){
+      $("#pumpAffinityPic").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#motorTechnologyTitle").click(function(){
+      $("#motorTechnologyPic").show();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#pumpPayingBackTitle").click(function(){
+      $("#pumpPayingBackPic").show();
+    });
+  });
